d818ef032f9caa1445e6a3eac52f8996a0a83ab4
Armando <armandoramossanchez@gmail.com>
2021-09-30 12:14:33 +0200

    quito settimeout de los ataques, y añado personajes a la pantalla del juego

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ class Skill{
 // CREAMOS LA CLASE CHARACTER DONDE DEFINIMOS LOS PERSONAJES
 class Character {
 
-    constructor(id, name, description, hp, mp, attack, defense, speed, skill1, skill2, skill3, skill4, img){
+    constructor(id, name, description, hp, mp, attack, defense, speed, skill1, skill2, skill3, skill4, img, imgWalking){
         this.id = id,
         this.name = name,
         this.description = description,
@@ -51,7 +51,8 @@ class Character {
         this.defense = defense,
         this.speed = speed,
         this.skills = [skill1, skill2, skill3, skill4],
-        this.img = img
+        this.img = img,
+        this.imgWalking = imgWalking
     }
 
 }
@@ -84,20 +85,20 @@ var alaDragon = new Skill("Ala de Dragón", "Golpea con el ala a 2 enemigos alea
 alaDragon.getType("double");
 
 // CREACION DE PERSONAJES
-var elementalist = new Character("elementalist", "Gloy Stylish, the Elementalist", "Es una rana, si. Y es un mago también. Usa hechizos elementales con daño masivo, y que provocan estados alterados.", 1500, 2000, 9, 1, 4, fireHorse, iceSpear, stormHammer, windKnife, "./img/elementalist.gif");
-var assassin = new Character("assassin", "Nia Noltan, the Assassin", "Asesina entrenada en artes marciales y tecnicas de veneno", 1750, 1000, 10, 2, 9, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, deathStrike, "./img/assassin.gif");
-var demon = new Character("demon", "Liura Blake, the Demon", "No sabemos porque nos quiere ayudar. Lo que sí sabemos es que le gusta la pelea", 3200, 1500, 7, 5, 6, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/demon.gif");
-var robot = new Character("robot", "BX109 - v1.023, the Robot", "Dice que viene de nosequé año para salvar a nosequién de una revolución de nosecuando... Aguanta buenos golpes.", 5600, 900, 5, 8, 4, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/robot.gif");
-var light = new Character("light", "Liskanor Tein, the Sun's Soldier", "Dice que es hijo del Sol, que le dio todos sus poderes y habilidades... No se, suena a cuento. Pero demonios, es fuerte.", 4500, 1250, 8, 3, 6, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/light.gif");
-var healer = new Character("healer", "Sue Giys, the Healer", "Ni idea de donde saca esas medicinas, pero te deja como nuevo.", 3250, 2000, 4, 4, 3, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/healer.gif");
-var angel = new Character("angel", "Flixinia Goltric, the Angel", "Bajada del cielo para echarnos una mano. No le gustan los malos, no necesita más excusas.", 5000, 2250, 3, 7, 8, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/angel.gif");
-var knight = new Character("knight", "Lucius Morrigan, the Shadow Knight", "No parece muy amigable, pero dice que quiere echar una mano en compensación de todo lo que hizo en el pasado.", 4800, 650, 7, 6, 2, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/knight.gif");
+var elementalist = new Character("elementalist", "Gloy Stylish, the Elementalist", "Es una rana, si. Y es un mago también. Usa hechizos elementales con daño masivo, y que provocan estados alterados.", 1500, 2000, 9, 1, 4, fireHorse, iceSpear, stormHammer, windKnife, "./img/elementalist.gif", "./img/elementalist-walking.gif");
+var assassin = new Character("assassin", "Nia Noltan, the Assassin", "Asesina entrenada en artes marciales y tecnicas de veneno", 1750, 1000, 10, 2, 9, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, deathStrike, "./img/assassin.gif", "./img/robot-walking.gif");
+var demon = new Character("demon", "Liura Blake, the Demon", "No sabemos porque nos quiere ayudar. Lo que sí sabemos es que le gusta la pelea", 3200, 1500, 7, 5, 6, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/demon.gif", "./img/robot-walking.gif");
+var robot = new Character("robot", "BX109 - v1.023, the Robot", "Dice que viene de nosequé año para salvar a nosequién de una revolución de nosecuando... Aguanta buenos golpes.", 5600, 900, 5, 8, 4, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/robot.gif", "./img/robot-walking.gif");
+var light = new Character("light", "Liskanor Tein, the Sun's Soldier", "Dice que es hijo del Sol, que le dio todos sus poderes y habilidades... No se, suena a cuento. Pero demonios, es fuerte.", 4500, 1250, 8, 3, 6, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/light.gif", "./img/robot-walking.gif");
+var healer = new Character("healer", "Sue Giys, the Healer", "Ni idea de donde saca esas medicinas, pero te deja como nuevo.", 3250, 2000, 4, 4, 3, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/healer.gif", "./img/healer-walking.gif");
+var angel = new Character("angel", "Flixinia Goltric, the Angel", "Bajada del cielo para echarnos una mano. No le gustan los malos, no necesita más excusas.", 5000, 2250, 3, 7, 8, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/angel.gif", "./img/robot-walking.gif");
+var knight = new Character("knight", "Lucius Morrigan, the Shadow Knight", "No parece muy amigable, pero dice que quiere echar una mano en compensación de todo lo que hizo en el pasado.", 4800, 650, 7, 6, 2, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/knight.gif", "./img/knight-walking.gif");
 
 var personajePrueba1 = new Character("personajePrueba1", "prueba1", "prueba", 1000, 1000, 10, 5, 5, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/elementalist.gif");
 var personajePrueba2 = new Character("personajePrueba2", "prueba2", "prueba", 1000, 1000, 10, 5, 5, habilidadPrueba2, habilidadPrueba1, habilidadPrueba3, habilidadPrueba4, "./img/elementalist.gif");
 var personajePrueba3 = new Character("personajePrueba3", "prueba3", "prueba", 1000, 1000, 10, 5, 5, habilidadPrueba3, habilidadPrueba2, habilidadPrueba1, habilidadPrueba4, "./img/elementalist.gif");
 
-var dragon = new Character("dragon", "Goliath", "Dragón que ha decidido acampar en el campo del pueblo. Está enfadado y no atiende a razones.", 10000, 5000, 500, 8, 8, alientoDragon, espinasDragon, placajeDragon, alaDragon, "./img/elementalist.gif");
+var dragon = new Character("dragon", "Goliath", "Dragón que ha decidido acampar en el campo del pueblo. Está enfadado y no atiende a razones.", 10000, 5000, 500, 8, 8, alientoDragon, espinasDragon, placajeDragon, alaDragon, "./img/elementalist.gif", "./img/dragon-walking.gif");
 
 // VARIABLES PARA CONTROLAR CUAL PARTE DE LA APLICACION SE VE
 var showGame = false;
@@ -108,13 +109,17 @@ var seleccionPersonajes = document.getElementById("seleccion-personajes");
 // FUNCION PARA ALTERNAR ENTRE PANTALLAS DE LA APLICACION Y PARA MOSTRAR LAS HABILIDADES DE LOS PERSONAJES ELEGIDOS
 const cambiarPantalla = () => {
     if (!showGame) {
+        let escenarioBatalla = document.getElementById("escenario-batalla");
+        let heroesBatalla = document.getElementById("heroes-en-batalla");
         pantallaJuego.style.display = "flex";
         seleccionPersonajes.style.display = "none";
         document.getElementById("switch").style.transform = "scaleY(0)";
         // PINTA TODOS LAS HABILIDADES DE LOS PERSONAJES EN LA PANTALLA DEL JUEGO
         for (let i = 0; i < personajesElegidos.length; i++) {
             muestraHabilidades(personajesElegidos[i], i);
+            heroesBatalla.innerHTML = heroesBatalla.innerHTML + `<div><img src="${personajesElegidos[i].imgWalking}"></div>`
         }
+        escenarioBatalla.innerHTML = escenarioBatalla.innerHTML + `<div id="enemigo-en-batalla"><img src="${dragon.imgWalking}"></div>`
     }else{
         pantallaJuego.style.display = "none";
         seleccionPersonajes.style.display = "flex";
@@ -181,7 +186,6 @@ const muestraHabilidades = (objetoPersonaje, posicionObjeto) => {
 // GUARDARÁ EN UN ARRAY EL PERSONAJE Y LA HABILIDAD QUE VA A USAR
 // TAMBIEN QUITAMOS LOS EVENTOS DE CLICK EN EL DIV CONTENEDOR DE LA LISTA DE HABILIDADES A LA QUE PERTENECE LA HABILIDAD
 // QUE ELEGIMOS, Y LE QUITAMOS OPACIDAD
-
 var habilidadesTurno = [];
 
 const guardaHabilidades = (personaje, skill, id) => {
@@ -195,52 +199,60 @@ const guardaHabilidades = (personaje, skill, id) => {
 const empezarTurno = () =>{
     document.getElementById("pantalla-empezar-turno").style.display = "none";
     document.getElementById("boton-terminar-turno").style.display = "block"
+    let radios = document.getElementsByTagName("input");
+    for (let i = 0; i < radios.length; i++) {
+        radios[i].checked = false;    
+    }
 }
-
 
 // FUNCION QUE EJECUTA LAS HABILIDADES DE NUESTROS PERSONAJES, ASI COMO DE NUESTRO ENEMIGO
 // EN LA FUNCION SE INVOCA A LOS VALORES DEL ARRAY DONDE GUARDAMOS CUALES HABILIDADES USAR, Y SE EJECUTA
 // CADA UNA DE LAS HABILIDADES
 // TAMBIEN SE RANDOMIZA EL USO DE HABILIDADES DEL ENEMIGO. EL ENEMIGO LANZARA 2 HABILIDADES EN EL MISMO TURNO DE LAS 4 QUE
 // TIENE EN SU PROPIA LISTA DE HABILIDADES
+// USAMOS SETTIMEOUT PARA VOLVER A LA PANTALLA DE INICIAR MENÚ
 const terminarTurno = () => {
+    document.getElementById("boton-terminar-turno").style.display = "none"
     // LANZAMIENTO DE HABILIDADES DE NUESTROS 4 PERSONAJES
     for (let i = 0; i < habilidadesTurno.length; i++) {
-        document.getElementById("boton-terminar-turno").style.display = "none"
-        setTimeout(() => {
             personajesElegidos[habilidadesTurno[i][0]].skills[habilidadesTurno[i][1]].useSkill(personajesElegidos[i], dragon);
             console.log("Uso la habilidad " + personajesElegidos[habilidadesTurno[i][0]].skills[habilidadesTurno[i][1]].name + " de " + personajesElegidos[i].name + ". Quedan " + personajesElegidos[i].mp + " puntos de maná");
             console.log("La vida del dragón es " + dragon.hp);
-        }, parseInt(20000 / personajesElegidos[habilidadesTurno[i][0]].speed));
     }
     // LANZAMIENTO DE HABILIDADES DEL DRAGON
     for (let i = 0; i < 2; i++) {
-        setTimeout(() => {
-            let skillNumber = parseInt(Math.random() * (4 - 0));
-            console.log("Dragon usa el ataque " + dragon.skills[skillNumber].name);
-            switch (dragon.skills[skillNumber].type) {
-                case "single":
-                    let target = parseInt(Math.random() * (4 - 0));
-                    dragon.skills[skillNumber].useSkill(dragon, personajesElegidos[target]);
-                    console.log("Hiere a " + personajesElegidos[target].name);
-                    break;
-                case "double":
-                    let target1 = parseInt(Math.random() * (4 - 0));
-                    let target2 = parseInt(Math.random() * (4 - 0));
-                    while (target1 == target2) {
-                        target2 = parseInt(Math.random() * (4 - 0));
-                    }
-                    dragon.skills[skillNumber].useSkillDouble(dragon, personajesElegidos[target1], personajesElegidos[target2]);
-                    console.log("Hiere a " + personajesElegidos[target1].name + " y a " + personajesElegidos[target2].name);
-                    break;
-                case "all":
-                    dragon.skills[skillNumber].useSkillAll(dragon, personajesElegidos[0], personajesElegidos[1], personajesElegidos[2], personajesElegidos[3]);
-                    console.log("Hiere a " + personajesElegidos[0].name + ", a " + personajesElegidos[1].name + ", a " + personajesElegidos[2].name + " y a " + personajesElegidos[3].name);
-                    break;
-            
-                default:
-                    break;
-            }
-        }, parseInt(20000 / dragon.speed));
+        let skillNumber = parseInt(Math.random() * (4 - 0));
+        console.log("Dragon usa el ataque " + dragon.skills[skillNumber].name);
+        switch (dragon.skills[skillNumber].type) {
+            case "single":
+                let target = parseInt(Math.random() * (4 - 0));
+                dragon.skills[skillNumber].useSkill(dragon, personajesElegidos[target]);
+                console.log("Hiere a " + personajesElegidos[target].name);
+                break;
+            case "double":
+                let target1 = parseInt(Math.random() * (4 - 0));
+                let target2 = parseInt(Math.random() * (4 - 0));
+                while (target1 == target2) {
+                    target2 = parseInt(Math.random() * (4 - 0));
+                }
+                dragon.skills[skillNumber].useSkillDouble(dragon, personajesElegidos[target1], personajesElegidos[target2]);
+                console.log("Hiere a " + personajesElegidos[target1].name + " y a " + personajesElegidos[target2].name);
+                break;
+            case "all":
+                dragon.skills[skillNumber].useSkillAll(dragon, personajesElegidos[0], personajesElegidos[1], personajesElegidos[2], personajesElegidos[3]);
+                console.log("Hiere a " + personajesElegidos[0].name + ", a " + personajesElegidos[1].name + ", a " + personajesElegidos[2].name + " y a " + personajesElegidos[3].name);
+                break;
+        
+            default:
+                break;
+        }
     }
+    setTimeout(() => {
+        document.getElementById("pantalla-empezar-turno").style.display = "flex";
+        habilidadesTurno = [];
+        for (let i = 0; i < 4; i++) {
+            document.getElementById(`character${i}`).style.pointerEvents = "initial";
+            document.getElementById(`character${i}`).style.opacity = "1";            
+        }
+    }, 500);
 }
