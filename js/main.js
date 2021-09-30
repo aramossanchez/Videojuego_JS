7a65c3aba2df628d14ee0960ceac6b2c2294bfd2
Armando <armandoramossanchez@gmail.com>
2021-09-30 17:59:46 +0200

    añado barras de salud y vida variables en funcion de los puntos que falten de cada stat. gestiono mejor la presentacion de div con eventos

--- js/main.js
+++ js/main.js
@@ -57,15 +57,32 @@ class Character {
 
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // CREACIÓN DE HABILIDADES
 
+//HABILIDADES DE ELEMENTALIST
 var fireHorse = new Skill("Caballo de fuego", "Hechizo que libera un caballo de fuego hacia el enemigo. Puede quemar", 500, "quemadura", 100);
 var iceSpear = new Skill("Lanza de hielo", "Hechizo que lanza una lanza de hielo. Puede congelar", 400, "congelar", 120);
 var stormHammer = new Skill("Martillo de la tormenta", "Hechizo que invoca un martillo hecho de truenos desde el cielo. Puede ralentizar", 800, "ralentizar", 250);
 var windKnife = new Skill("Cuchillo de viento", "Hechizo que invoca corrientes de viento cortantes", 350, "sangrar", 100);
 
+//HABILIDADES DE ASSASSIN
 var deathStrike = new Skill("Golpe mortal", "Ataque que puede matar de un solo golpe, pero tiene muy poca probabilidad de acertar", 10000, "", 200);
+
+//HABILIDADES DE DEMON
+
+//HABILIDADES DE ROBOT
+
+//HABILIDADES DE SUN'S SOLDIER
+
+//HABILIDADES DE HEALER
+
+//HABILIDADES DE ANGEL
+
+//HABILIDADES DE SHADOW KNIGHT
 
 var habilidadPrueba1 = new Skill("prueba1", "prueba", 500, "", 50);
 var habilidadPrueba2 = new Skill("prueba2", "prueba", 500, "", 50);
@@ -85,51 +102,39 @@ var alaDragon = new Skill("Ala de Dragón", "Golpea con el ala a 2 enemigos alea
 alaDragon.getType("double");
 
 // CREACION DE PERSONAJES
+
+//PERSONAJES ELEGIBLES
 var elementalist = new Character("elementalist", "Gloy Stylish, the Elementalist", "Es una rana, si. Y es un mago también. Usa hechizos elementales con daño masivo, y que provocan estados alterados.", 1500, 2000, 9, 1, 4, fireHorse, iceSpear, stormHammer, windKnife, "./img/elementalist.gif", "./img/elementalist-walking.gif");
+
 var assassin = new Character("assassin", "Nia Noltan, the Assassin", "Asesina entrenada en artes marciales y tecnicas de veneno", 1750, 1000, 10, 2, 9, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, deathStrike, "./img/assassin.gif", "./img/assassin-walking.gif");
+
 var demon = new Character("demon", "Liura Blake, the Demon", "No sabemos porque nos quiere ayudar. Lo que sí sabemos es que le gusta la pelea", 3200, 1500, 7, 5, 6, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/demon.gif", "./img/demon-walking.gif");
+
 var robot = new Character("robot", "BX109 - v1.023, the Robot", "Dice que viene de nosequé año para salvar a nosequién de una revolución de nosecuando... Aguanta buenos golpes.", 5600, 900, 5, 8, 4, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/robot.gif", "./img/robot-walking.gif");
+
 var light = new Character("light", "Liskanor Tein, the Sun's Soldier", "Dice que es hijo del Sol, que le dio todos sus poderes y habilidades... No se, suena a cuento. Pero demonios, es fuerte.", 4500, 1250, 8, 3, 6, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/light.gif", "./img/light-walking.gif");
+
 var healer = new Character("healer", "Suerestil Giysh, the Healer", "Ni idea de donde saca esas medicinas, pero te deja como nuevo.", 3250, 2000, 4, 4, 3, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/healer.gif", "./img/healer-walking.gif");
+
 var angel = new Character("angel", "Flixinia Goltric, the Angel", "Bajada del cielo para echarnos una mano. No le gustan los malos, no necesita más excusas.", 5000, 2250, 3, 7, 8, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/angel.gif", "./img/angel-walking.gif");
+
 var knight = new Character("knight", "Lucius Morrigan, the Shadow Knight", "No parece muy amigable, pero dice que quiere echar una mano en compensación de todo lo que hizo en el pasado.", 4800, 650, 7, 6, 2, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/knight.gif", "./img/knight-walking.gif");
 
-var personajePrueba1 = new Character("personajePrueba1", "prueba1", "prueba", 1000, 1000, 10, 5, 5, habilidadPrueba1, habilidadPrueba2, habilidadPrueba3, habilidadPrueba4, "./img/elementalist.gif");
-var personajePrueba2 = new Character("personajePrueba2", "prueba2", "prueba", 1000, 1000, 10, 5, 5, habilidadPrueba2, habilidadPrueba1, habilidadPrueba3, habilidadPrueba4, "./img/elementalist.gif");
-var personajePrueba3 = new Character("personajePrueba3", "prueba3", "prueba", 1000, 1000, 10, 5, 5, habilidadPrueba3, habilidadPrueba2, habilidadPrueba1, habilidadPrueba4, "./img/elementalist.gif");
-
+//ENEMIGO
 var dragon = new Character("dragon", "Goliath", "Dragón que ha decidido acampar en el campo del pueblo. Está enfadado y no atiende a razones.", 10000, 5000, 500, 8, 8, alientoDragon, espinasDragon, placajeDragon, alaDragon, "./img/elementalist.gif", "./img/dragon-walking.gif");
 
-// VARIABLES PARA CONTROLAR CUAL PARTE DE LA APLICACION SE VE
-var showGame = false;
-
-var pantallaJuego = document.getElementById("pantalla-juego");
-var seleccionPersonajes = document.getElementById("seleccion-personajes");
-
-// FUNCION PARA ALTERNAR ENTRE PANTALLAS DE LA APLICACION Y PARA MOSTRAR LAS HABILIDADES DE LOS PERSONAJES ELEGIDOS
-// PINTAMOS TAMBIEN A LOS PERSONAJES EN PANTALLA
-const cambiarPantalla = () => {
-    if (!showGame) {
-        let escenarioBatalla = document.getElementById("escenario-batalla");
-        let heroesBatalla = document.getElementById("heroes-en-batalla");
-        pantallaJuego.style.display = "flex";
-        seleccionPersonajes.style.display = "none";
-        document.getElementById("switch").style.transform = "scaleY(0)";
-        // PINTA TODOS LAS HABILIDADES DE LOS PERSONAJES EN LA PANTALLA DEL JUEGO
-        for (let i = 0; i < personajesElegidos.length; i++) {
-            muestraHabilidades(personajesElegidos[i], i);
-            heroesBatalla.innerHTML = heroesBatalla.innerHTML + `<div><img src="${personajesElegidos[i].imgWalking}"></div>`
-        }
-        escenarioBatalla.innerHTML = escenarioBatalla.innerHTML + `<div id="enemigo-en-batalla"><img src="${dragon.imgWalking}"></div>`
-    }else{
-        pantallaJuego.style.display = "none";
-        seleccionPersonajes.style.display = "flex";
-    }
-    showGame = !showGame;
-}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // EN ESTE ARRAY GUARDAMOS TODOS LOS PERSONAJES ELEGIBLES
 var todosLosPersonajes = [elementalist, assassin, demon, robot, light, healer, angel, knight];
+
+// MOSTRAMOS PERSONAJES EN LA PANTALLA DE SELECCION DE PERSONAJES
+let personaje = document.getElementById("seleccion-personajes");
+for (let i = 0; i < todosLosPersonajes.length; i++) {
+    personaje.innerHTML = personaje.innerHTML + `<div id="${todosLosPersonajes[i].id}" class="personaje-para-elegir" onclick="guardarPersonajeElegido('${i}', '${todosLosPersonajes[i].id}')"><h1>${todosLosPersonajes[i].name}</h1><img src="${todosLosPersonajes[i].img}"><p>Life: ${todosLosPersonajes[i].hp}</p><p>Mana: ${todosLosPersonajes[i].mp}</p><p>Attack: ${todosLosPersonajes[i].attack}</p><p>Defense: ${todosLosPersonajes[i].defense}</p></div>`;
+}
 
 // EN ESTE ARRAY GUARDAMOS LOS PERSONAJES ELEGIDOS
 var personajesElegidos = [];
@@ -155,11 +160,63 @@ const guardarPersonajeElegido = (lugarArray, id) =>{
     }
 }
 
-// MOSTRAMOS PERSONAJES EN LA PANTALLA DE SELECCION DE PERSONAJES
-let personaje = document.getElementById("seleccion-personajes");
-for (let i = 0; i < todosLosPersonajes.length; i++) {
-    personaje.innerHTML = personaje.innerHTML + `<div id="${todosLosPersonajes[i].id}" class="personaje-para-elegir" onclick="guardarPersonajeElegido('${i}', '${todosLosPersonajes[i].id}')"><h1>${todosLosPersonajes[i].name}</h1><img src="${todosLosPersonajes[i].img}"><p>Life: ${todosLosPersonajes[i].hp}</p><p>Mana: ${todosLosPersonajes[i].mp}</p><p>Attack: ${todosLosPersonajes[i].attack}</p><p>Defense: ${todosLosPersonajes[i].defense}</p></div>`;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// VARIABLES PARA CONTROLAR CUAL PARTE DE LA APLICACION SE VE
+var showGame = false;
+
+var pantallaJuego = document.getElementById("pantalla-juego");
+var seleccionPersonajes = document.getElementById("seleccion-personajes");
+
+
+// VARIABLE EN LA QUE GUARDAMOS EL ELEMENTO CONTENEDOR DE LAS BARRAS DE SALUD Y MANA
+let barrasSaludMana = document.getElementById("barras-salud-mana");
+
+// CREAMOS ARRAY PARA GUARDAR LA VIDA Y EL MANA DE LOS PERSONAJES SELECCIONADOS, NECESARIO PARA PODER MOSTRAR CUANTA 
+// VIDA Y MANA SE HA PERDIDO EN LAS BARRAS DE SALUD Y DE MANA
+let hpPersonajesElegidos = [];
+let mpPersonajesElegidos = [];
+
+// CON ESTA FUNCION PINTAMOS LAS BARRAS DE SALUD Y MANA, Y ACTUALIZAMOS LONGITUD DE BARRAS EN FUNCION DE LOS PUNTOS QUE FALTEN
+// LA LLAMAMOS AL INICIAR EL JUEGO Y DESPUES DE CADA TURNO
+const pintarBarrasSaludMana = () =>{
+    for (let i = 0; i < personajesElegidos.length; i++) {
+        barrasSaludMana.innerHTML = barrasSaludMana.innerHTML + `<div class="barras-de-personaje"><h2>${personajesElegidos[i].name}</h2><div class="texto-barra-salud">${personajesElegidos[i].hp}/${hpPersonajesElegidos[i]}<div class="barra-salud" style="width: ${porcentajeSalud(hpPersonajesElegidos[i], personajesElegidos[i].hp)}%"></div></div><div class="texto-barra-mana">${personajesElegidos[i].mp}/${mpPersonajesElegidos[i]}</div></div>`
+    }
 }
+
+// FUNCION PARA ALTERNAR ENTRE PANTALLAS DE LA APLICACION Y PARA MOSTRAR LAS HABILIDADES DE LOS PERSONAJES ELEGIDOS
+// PINTAMOS TAMBIEN A LOS PERSONAJES EN PANTALLA
+const iniciarJuego = () => {
+    // GUARDAMOS EN ARRAY LA VIDA Y EL MANA DE LOS PERSONAJES SELECCIONADOS
+    for (let i = 0; i < 4; i++) {
+        hpPersonajesElegidos.push(personajesElegidos[i].hp);
+        mpPersonajesElegidos.push(personajesElegidos[i].mp);
+    }
+    console.log(hpPersonajesElegidos, mpPersonajesElegidos);
+    if (!showGame) {
+        let escenarioBatalla = document.getElementById("escenario-batalla");
+        let heroesBatalla = document.getElementById("heroes-en-batalla");
+        pantallaJuego.style.display = "flex";
+        seleccionPersonajes.style.display = "none";
+        document.getElementById("switch").style.transform = "scaleY(0)";
+        // PINTA LA VIDA Y LA SALUD DE LOS PERSONAJES EN LA PANTALLA DE JUEGO
+        pintarBarrasSaludMana();
+        // PINTA TODOS LAS HABILIDADES DE LOS PERSONAJES EN LA PANTALLA DEL JUEGO
+        for (let i = 0; i < personajesElegidos.length; i++) {
+            muestraHabilidades(personajesElegidos[i], i);
+            heroesBatalla.innerHTML = heroesBatalla.innerHTML + `<div><img src="${personajesElegidos[i].imgWalking}"></div>`
+        }
+        escenarioBatalla.innerHTML = escenarioBatalla.innerHTML + `<div id="enemigo-en-batalla"><img src="${dragon.imgWalking}"></div>`
+    }else{
+        pantallaJuego.style.display = "none";
+        seleccionPersonajes.style.display = "flex";
+    }
+    showGame = !showGame;
+}
+
 
 // CREAMOS ARRAY PARA GUARDAR LAS HABILIDADES QUE MOSTRAREMOS EN CADA LISTA DE HABILIDADES
 var habilidades = [];
@@ -195,20 +252,17 @@ const guardaHabilidades = (personaje, skill, id) => {
     habilidadesTurno.push([personaje, skill]);
 }
 
-// FUNCION QUE CARGA LA PANTALLA DEL JUEGO, CON TODAS LAS HABILIDADES DE LOS PERSONAJES ELEGIDOS Y SUS BARRAS DE VIDA Y MANA
-// TAMBIEN CARGAR EL BOTON DE TERMINAR TURNO
-let barrasSaludMana = document.getElementById("barras-salud-mana");
-
+// FUNCION QUE MUESTRA LA PANTALLA DEL JUEGO, CON TODAS LAS HABILIDADES DE LOS PERSONAJES ELEGIDOS SIN CHECKED, 
+// SUS BARRAS DE VIDA Y MANA Y EL DIV CONTENEDOR DE SUS HABILIDADES
+// TAMBIEN CARGA EL BOTON DE TERMINAR TURNO
 const empezarTurno = () =>{
-    barrasSaludMana.innerHTML = "";
     document.getElementById("pantalla-empezar-turno").style.display = "none";
     document.getElementById("boton-terminar-turno").style.display = "block";
+    document.getElementById("habilidades-personajes").style.display = "flex";
+    document.getElementById("barras-salud-mana").style.display = "flex";
     let radios = document.getElementsByTagName("input");
     for (let i = 0; i < radios.length; i++) {
         radios[i].checked = false;    
-    }
-    for (let i = 0; i < personajesElegidos.length; i++) {
-        barrasSaludMana.innerHTML = barrasSaludMana.innerHTML + `<div class="barras-de-personaje"><h2>${personajesElegidos[i].name}</h2><div class="barra-salud">${personajesElegidos[i].hp}</div><div class="barra-mana">${personajesElegidos[i].mp}</div></div>`
     }
 }
 
@@ -254,13 +308,21 @@ const terminarTurno = () => {
                 break;
         }
     }
+    // ESTO SE EJECUTARÁ TRAS TODOS LOS ATAQUES DE LOS PERSONAJES ELEGIDOS Y DEL ENEMIGO
     setTimeout(() => {
         document.getElementById("pantalla-empezar-turno").style.display = "flex";
         habilidadesTurno = [];
+        // HABILITAMOS DE NUEVO EL USO DE HABILIDADES
         for (let i = 0; i < 4; i++) {
             document.getElementById(`character${i}`).style.pointerEvents = "initial";
             document.getElementById(`character${i}`).style.opacity = "1";            
         }
-        
+        // PINTAMOS DE NUEVO LA VIDA Y EL MANA DE CADA PERSONAJE
+        barrasSaludMana.innerHTML = "";
+        pintarBarrasSaludMana();
     }, 500);
+}
+
+const porcentajeSalud = (vidaTotal, vidaActual) =>{
+    return parseInt(vidaActual/vidaTotal * 100);
 }
